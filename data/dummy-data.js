856a94afcbc308f242815226ec1652c229edaafc
DwanW <kunvang@yahoo.com>
2021-03-19 21:12:50 -0600

    upgraded package to match script version

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -4,7 +4,7 @@ import Meal from '../models/meal';
 export const CATEGORIES = [
   new Category('c1', 'Italian', '#f5428d'),
   new Category('c2', 'Quick & Easy', '#f54242'),
-  new Category('c3', 'Hamburgers', '#f5a442'),
+  new Category('c3', 'Hamburger', '#f5a442'),
   new Category('c4', 'German', '#f5d142'),
   new Category('c5', 'Light & Lovely', '#368dff'),
   new Category('c6', 'Exotic', '#41d95d'),
